32a553a56a918827479959c5532a7d6f9e2fcf66
valquaint <valquaint@gmail.com>
2023-10-25 01:29:06 +0000

    Fixed validation page

--- src/pages/SuccessPage/SuccessPage.js
+++ src/pages/SuccessPage/SuccessPage.js
@@ -1,16 +1,25 @@
-import { useParams } from "react-router-dom"
-import "../../services/auth"
+import { useSearchParams } from "react-router-dom"
+import * as auth from "../../services/auth"
+import { useEffect } from "react";
+import { useState } from "react";
 
 const SuccessPage = () => {
-    const params = useParams()
-    const resultId = params.symbol
-    const validator = params.symbol
-    const url = `https://dev.pawson.life/confirmRegister?id=${resultId}&validation=${validator}`
-    
-    return(
+    const [searchParams] = useSearchParams();
+    const [valid, setValid] = useState(false);
+    const id = searchParams.get("id");
+    const validator = searchParams.get("validation");
+    //const url = `https://dev.pawson.life/confirmRegister?id=${resultId}&validation=${validator}`
+    useEffect( () => {
+        async function verify () {
+            const isValid = await auth.VerifyUser(id, validator);
+            setValid(isValid);
+        }
+        verify();
+    })
+    return valid ? (
         <h1>success</h1>
         
 
-    )
+    ) : ( <h1>Fail. GTFO</h1>)
 }
 export default SuccessPage
