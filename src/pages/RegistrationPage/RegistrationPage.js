f71edd0247ef50e5850aabcaad63250802d6a537
Ian <ianpmaher@gmail.com>
2023-11-18 20:52:41 -0500

    puppy background to login and register

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -5,6 +5,9 @@ const RegistrationPageContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    background: url("https://images.unsplash.com/photo-1526510335242-248dc3765086?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    background-size: stretch;
+    background-position: bottom;
 `
 
 
